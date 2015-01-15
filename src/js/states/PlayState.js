d18591106b3941745f8d9b29e38c666ced3852e6
a-ghost-fart <a-ghost-fart@fedora.sexy>
2015-01-15 15:55:26 +0000

    speed

--- src/js/states/PlayState.js
+++ src/js/states/PlayState.js
@@ -69,11 +69,11 @@ module.exports = {
         this.player.angle = 0;
 
         if (this.game.input.keyboard.isDown(Phaser.Keyboard.A)) {
-            this.player.body.velocity.x = -150;
+            this.player.body.velocity.x = -250;
             this.player.angle = -10;
         }
         if (this.game.input.keyboard.isDown(Phaser.Keyboard.D)) {
-            this.player.body.velocity.x = 150;
+            this.player.body.velocity.x = 250;
             this.player.angle = 10;
         }
         if (this.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR) && this.player.body.onFloor()) {
@@ -84,7 +84,7 @@ module.exports = {
             var bullet = this.projectiles.getFirstDead();
             bullet.reset(this.player.x, this.player.y);
             bullet.rotation = this.game.physics.arcade.moveToObject(this, this.game.input.activePointer, 500);
-        };
+        }
 
     },
 
